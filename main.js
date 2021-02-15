aecd93d42d3e618a739196a53d4f726765734082
Mirco Wittrien <mirco.wittrien@gmail.com>
2021-02-15 12:22:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ window.onload = function () {
 	url = url.startsWith("https://") || url.startsWith("http://") ? url : `https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/${url}`;
 	if (url.indexOf("raw.githubusercontent.com") == -1 && url.indexOf("github.io") == -1) return error(`<a>${url}</a> not a valid GitHub File URL!`);
 	const xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
+	xhttp.onload = function() {
+		if (this.status == 200) {
 			const tempLink = document.createElement("a");
 			tempLink.href = window.URL.createObjectURL(new Blob([this.response]));
 			tempLink.download = url.split("/").pop();
 			tempLink.click();
 		}
+		if (this.status == 404) error(`GitHub File <a>${url}</a> does not exist!`);
 	};
 	xhttp.onerror = function() {error(`GitHub File <a>${url}</a> does not exist!`);};
 	xhttp.open("GET", url, true);
