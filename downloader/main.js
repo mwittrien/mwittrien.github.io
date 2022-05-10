d3ab2767ee18477b8423e06a1027f8554d0e0133
Mirco Wittrien <23700969+mwittrien@users.noreply.github.com>
2022-05-10 11:58:26 +0200

    stuff

--- downloader/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-	const error = text => {
-		document.title = "Downloader - Failed";
-		document.body.innerHTML = text;
-	};
-	if (!window.location.search) error("No Parameters");
-	else window.DownloadApi.convert(window.location.search, error);
-};
